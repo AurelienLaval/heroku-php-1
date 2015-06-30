f491dd4cb031df48d33e2795056db4e2e72417db
Aurélien Laval <aurelien.laval@gmail.com>
2015-06-30 18:15:42 +0200

    Update createContact.php

--- app/createContact.php
+++ app/createContact.php
@@ -19,11 +19,32 @@
 				$records[0]->type = 'Contact';
 				
 				$response = $mySforceConnection->create($records);
+				?>
+				Contact correctement créé !<br/>
+				<table>
+					<tr>
+						<th>Id : </th>
+						<td><?php echo $response[0]->Id; ?></td>
+					</tr>
+					<tr>
+						<th>Nom : </th>
+						<td><?php echo $response[0]->LastName; ?></td>
+					</tr>
+					<tr>
+						<th>Prénom : </th>
+						<td><?php echo $response[0]->FirstName; ?></td>
+					</tr>
+					<tr>
+						<th>Téléphone : </th>
+						<td><?php echo $response[0]->Phone; ?></td>
+					</tr>
+				</table><br/>
+				<?php
 			}catch(Exception $e){
 				echo "Erreur à la création d'un contact :" . $e.getMessage();
 			}
 		?>
-
-		Bla bla !
+		
+		<a href="/index.php?username=<?php echo USERNAME; ?>&token=<?php echo SECURITY_TOKEN; ?">Retour à la page principale</a>
 	</body>
 </html>
